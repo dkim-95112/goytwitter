01fdc6d56a0319c276575052a39507e064054c30
David Kim <innerb@gmail.com>
2020-04-21 12:29:49 -0700

    Adding interceptor/guard files in '_helpers' directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // import { MatSliderModule } from '@angular/material/slider';
 import {MatCardModule} from '@angular/material/card';
-import { TweetCardComponent } from './tweet-card/tweet-card.component';
-import { TweetListComponent } from './tweet-list/tweet-list.component';
-
+import {AppComponent} from './app.component';
+import {TweetCardComponent} from './tweet-card/tweet-card.component';
+import {TweetListComponent} from './tweet-list/tweet-list.component';
+// used to create fake backend
+import { FakeBackendInterceptor } from './_helpers';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home/home.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    TweetCardComponent,
-    TweetListComponent
-  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
     HttpClientModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    TweetCardComponent,
+    TweetListComponent,
+    HomeComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    FakeBackendInterceptor
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
