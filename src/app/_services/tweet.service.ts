bc3a9a8a8c2e051910970724f2a12f2a3ea9e139
David Kim <innerb@gmail.com>
2020-04-25 14:45:34 -0700

    Adding websocket subject

--- src/app/_services/tweet.service.ts
+++ src/app/_services/tweet.service.ts
@@ -3,15 +3,32 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 import {CreateTweetResponse, Tweet} from '../_models';
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TweetService {
   tweetUrl = 'http://localhost:3000/tweets'; // 'assets/tweets.json';
+  socket$: WebSocketSubject<unknown>;
+
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
   ) {
+    this.socket$ = webSocket('ws://localhost:3000');
+    this.socket$.subscribe(
+      msg => {
+        // Called whenever there is a message from the server.
+        console.log('socket message received: %o', msg);
+      },
+      err => {
+        // Called if at any point WebSocket API signals some kind of error.
+        console.log(err);
+      },
+      () => {
+        console.log('complete'); // Called when connection is closed (for whatever reason).
+      }
+    );
   }
 
   private static handlerError(error: HttpErrorResponse) {
