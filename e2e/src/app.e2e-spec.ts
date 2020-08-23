46c1349a42db7bc674e813a685d4915d17f3baa5
dyk <innerb@gmail.com>
2020-08-23 11:38:40 -0400

    Changing name from 'goytwitter' to 'goytoot'

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('goytwitter app is running!');
+    expect(page.getTitleText()).toEqual('goytoot app is running!');
   });
 
   afterEach(async () => {
